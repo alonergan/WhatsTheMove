8c6f4dca0ad512f402953e5d65bb8ff6f9051ed0
alonergan <alonergan@wisc.edu>
2023-02-16 16:48:59 -0600

    New branch to test SQL Azure connection

---
+++ WhatsTheMoveApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatsTheMoveApp.Models
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static MobileServiceClient client
+    }
+}
